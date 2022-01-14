af5e198604221a9572af32817297d8343cc78107
angussov <94641116+angussov@users.noreply.github.com>
2022-01-13 22:01:36 -0300

    Update ApiController.java
    Fiz uma alteração direto no site do GitHub

--- src/main/java/br/com/vitoria/programas02/controller/ApiController.java
+++ src/main/java/br/com/vitoria/programas02/controller/ApiController.java
@@ -20,4 +20,9 @@ public class ApiController {
 	public String t() {
 		return "Aki é o T2!";
 	}
+	
+	@GetMapping("/t3")
+	public String t3() {
+		return "Aki é o T3 alterado direto no GitHub!";
+	}
 }
